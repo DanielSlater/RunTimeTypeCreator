fa420c64d41c966ec8f69b2078607bf6e026e916
Daniel Slater <danielslaterishere@gmail.com>
2015-05-20 22:52:51 +0100

    RunTimeTypeCreator
    First version

---
+++ RunTimeTypeCreator/RunTimeTypeCreator.Tests/RunTimeTypeCreatorTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using FluentAssert;
+using NUnit.Framework;
+
+namespace RunTimeTypeCreator.Tests
+{
+    [TestFixture]
+    public class RunTimeTypeCreatorTests
+    {
+        [Test]
+        public void CanLoadAndExecuteType()
+        {
+            const string source = @"
+using RunTimeTypeCreator.Tests;
+
+public class TestTypeClass : RunTimeTypeCreatorTests.ITestType
+{
+    public bool Success { get { return true; } }
+}";
+            List<string> compilationErrors;
+            var type = RunTimeTypeCreator.CreateType<ITestType>(source, 
+                new[]{"RunTimeTypeCreator.Tests.dll"}, 
+                out compilationErrors);
+            
+            type.Success.ShouldBeTrue();
+        }
+
+        public static bool TestVariable = false;
+
+        [Test]
+        public void CanAccessVariablesInThisDomain()
+        {
+            const string source = @"
+using RunTimeTypeCreator.Tests;
+
+public class TestTypeClass : RunTimeTypeCreatorTests.ITestType
+{
+    public bool Success { get { return RunTimeTypeCreatorTests.TestVariable; } }
+}";
+            List<string> compilationErrors;
+            var type = RunTimeTypeCreator.CreateType<ITestType>(source,
+                new[] { "RunTimeTypeCreator.Tests.dll" },
+                out compilationErrors);
+
+            TestVariable = false;
+            type.Success.ShouldBeFalse();
+            TestVariable = true;
+            type.Success.ShouldBeTrue();
+        }
+
+        [Test]
+        public void ReturnsCompilationErrors()
+        {
+            const string source = "this wont compile";
+
+            List<string> compilationErrors;
+            var type = RunTimeTypeCreator.CreateType<ITestType>(source,
+                new[] { "RunTimeTypeCreator.Tests.dll" },
+                out compilationErrors);
+
+            compilationErrors.ShouldNotBeNull();
+            compilationErrors.Count.ShouldBeGreaterThan(0);
+            type.ShouldBeNull();
+        }
+
+        public interface ITestType
+        {
+            bool Success { get; }
+        }
+    }
+}
+
---
+++ RunTimeTypeCreator/RunTimeTypeCreator/RunTimeTypeCreator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CSharp;
+
+namespace RunTimeTypeCreator
+{
+    public class RunTimeTypeCreator
+    {
+        public static T CreateType<T>(string source,
+            IEnumerable<string> assemblies,
+            out List<string> compilationErrors)
+            where T : class
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                throw new ArgumentException("Must provide valid source", "source");
+
+            compilationErrors = new List<string>();
+
+            var csc = new CSharpCodeProvider();
+            var parameters = new CompilerParameters
+            {
+                GenerateExecutable = false,
+                GenerateInMemory = true
+            };
+
+            if(assemblies != null)
+                foreach (var assembly in assemblies)
+                    parameters.ReferencedAssemblies.Add(assembly);
+
+            var result = csc.CompileAssemblyFromSource(parameters, source);
+
+            if (result.Errors.Count > 0)
+            {
+                compilationErrors.AddRange(from CompilerError compErr in result.Errors
+                                select string.Format("Line number {0}, Error Number: {1}, '{2}'",
+                                    compErr.Line, compErr.ErrorNumber, compErr.ErrorText));
+
+                return null;
+            }
+
+            //we compiled succesfully
+            //check for the type we want
+            var types = result.CompiledAssembly.GetTypes()
+                .Where(x => typeof(T).IsAssignableFrom(x)).ToList();
+
+            if (!types.Any())
+            {
+                compilationErrors.Add(string.Format("Could not find any types that implement {0} in source code",
+                    typeof(T).Name));
+                return null;
+            }
+            if (types.Count() > 1)
+            {
+                compilationErrors.Add(string.Format("Found multiple {0} in source. There should only be one {0}", typeof(T).Name));
+                return null;
+            }
+
+            var parameterlessConstructor = types.First().GetConstructor(new Type[] { });
+            if (parameterlessConstructor == null)
+            {
+                compilationErrors.Add(string.Format("{0} implementation must have a public parameterless constructor", typeof(T).Name));
+                return null;
+            }
+
+            //create the type and return
+            return (T)Activator.CreateInstance(types.First());
+        }
+    }
+}
